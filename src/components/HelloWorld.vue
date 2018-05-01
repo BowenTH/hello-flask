aefb8f4f2b7ed8cba4bef81b2a6ba9913d425b80
耍帅的兔子 <1578779013@qq.com>
2018-05-01 11:14:31 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App 你大爷永远是你大爷',
+      msg: 'Welcome to Your Vue.js App 你大爷永远是你大爷!',
       res: ''
     }
   },
